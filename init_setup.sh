3bf6b91f16abb3558a951b51bfc0a5343c2e681b
abhishekanand-02 <bgsabhishekanand02@gmail.com>
2025-01-16 17:25:02 +0530

    ipynb added

---
+++ init_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo [$(date)]: "START"
+
+echo [$(date)]: "creating env with python 3.12.8 version"
+
+python3 -m venv ./env
+
+echo [$(date)]: "activating the environment"
+
+source ./env/bin/activate
+
+echo [$(date)]: "installing the dev requirements"
+
+pip install -r requirements_dev.txt
+
+echo [$(date)]: "END"
