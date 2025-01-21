73d00062706f90db9493ddcccc76a8171d770b68
abhishekanand-02 <bgsabhishekanand02@gmail.com>
2025-01-21 11:29:56 +0530

    Initial commit

---
+++ stage_01.py
@@ -0,0 +1,6 @@
+with open("artifact.txt","w") as f:
+    f.write("My name is Abhishek and here I am executing stage 1.")
+
+
+with open("artifact2.txt","w") as f:
+    f.write("My name is Abhishek and this is my 2nd message.. .")
---
+++ stage_02.py
@@ -0,0 +1,8 @@
+with open("artifact.txt","r") as f:
+    text = f.read()
+print(text)
+
+print(" I am teaching dvc")
+
+
+print(" This is my 2nd example..")
